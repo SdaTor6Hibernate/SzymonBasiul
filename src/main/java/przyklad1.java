9f0f42584260936051059432413fb3f70ff91ef5
Szymon Basiul <szymon.basiul@wp.pl>
2020-03-29 10:41:23 +0200

    Entity BEZ carta

--- src/main/java/przyklad1.java
+++ src/main/java/przyklad1.java
@@ -1,2 +1,14 @@
+import model.*;
+import org.hibernate.Session;
+import util.HibernateUtil;
+
 public class przyklad1 {
+
+    public static void main(String[] args) {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        User user = session.find(User.class, 1);
+        session.close();
+        System.out.println(user.toString());
+    }
+
 }
