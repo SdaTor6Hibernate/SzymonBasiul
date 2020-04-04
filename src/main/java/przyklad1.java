8172192d4359bb542efe25b5add7e1d3d0373f0e
Szymon Basiul <szymon.basiul@wp.pl>
2020-04-04 14:32:04 +0200

    Blad model

--- src/main/java/przyklad1.java
+++ src/main/java/przyklad1.java
@@ -6,8 +6,8 @@ public class przyklad1 {
 
     public static void main(String[] args) {
         Session session = HibernateUtil.getSessionFactory().openSession();
-       // Product adress = session.find(Product.class, 1);
-       // System.out.println(adress.getDescription());
+        Product adress = session.find(Product.class, 1);
+        System.out.println(adress.getDescription());
         session.close();
 
     }
